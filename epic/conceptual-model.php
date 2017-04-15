c73d0a593c083b10f6fe284a7eba986ce79c3727
littleloveprint@gmail.com <littleloveprint@gmail.com>
2017-04-15 11:33:10 -0600

    Created drop tables, profile table, product table, and favorite table. Also restructured the conceptual model.

--- epic/conceptual-model.php
+++ epic/conceptual-model.php
@@ -13,13 +13,12 @@
 			<h2>Entities &amp; Attributes</h2>
 			<h3>Profile</h3>
 			<ul>
-				<li>profileSalt</li>
-				<li>profileHash</li>
 				<li>profileId</li>
-				<li>profileImage</li>
 				<li>profileUserName</li>
 				<li>profileLocation</li>
 				<li>profileJoinDate</li>
+				<li>profileHash</li>
+				<li>profileSalt</li>
 			</ul>
 			<h3>Product</h3>
 			<ul>
@@ -35,12 +34,11 @@
 				<li>favoriteProductId</li>
 				<li>favoriteDate</li>
 			</ul>
-			<p><strong>Subject: </strong>Courtney Thompson<br><strong>Verb: </strong>Favorites<br><strong>Object: </strong>Coozies</p>
-
 			<h2>Relationships</h2>
 			<ul>
 				<li>Many profiles can favorite many products.</li>
 			</ul>
+			<p><strong>Subject: </strong>Courtney Thompson<br><strong>Verb: </strong>Favorites<br><strong>Object: </strong>Coozies</p>
 		</main>
 	</body>
 </html>
---
+++ sql/data-design.sql
@@ -0,0 +1,51 @@
+DROP TABLE IF EXISTS 'favorite';
+DROP TABLE IF EXISTS 'product';
+DROP TABLE IF EXISTS 'profile';
+
+-- The profile entity.
+CREATE TABLE profile (
+		-- this creates the attribute for the primary key
+		-- auto_increment tells mySQL to number them {1, 2, 3, ...}
+		-- not null means the attribute is required!
+		profileId INT UNSIGNED AUTO_INCREMENT NOT NULL,
+		-- ^ Primary Key ^
+		profileUserName VARCHAR(32) NOT NULL,
+		profileLocation VARCHAR(64) NOT NULL,
+		profileJoinDate DATETIME(6) NOT NULL,
+		profileHash CHAR(128) NOT NULL,
+		profileSalt CHAR(64) NOT NULL,
+		UNIQUE(profileUserName),
+		PRIMARY KEY(profileId)
+);
+
+-- The product entity.
+CREATE TABLE product (
+		-- this is for another primary key...
+		productId INT UNSIGNED AUTO_INCREMENT NOT NULL,
+		productProfileId INT UNSIGNED NOT NULL,
+		productDescription VARCHAR(1000) NOT NULL,
+		productPrice VARCHAR(12) NOT NULL,
+		productPostDate DATETIME(6) NOT NULL,
+		INDEX(productProfileId),
+	-- ^ This creates an index before making a foreign key.
+		FOREIGN KEY(productProfileId) REFERENCES profile(profileId),
+	-- ^ This creates the actual foreign key relation.
+		PRIMARY KEY(productId)
+		-- ^ This creates the primary key.
+);
+
+-- The favorite entity.
+CREATE TABLE `favorite` (
+		favoriteProfileId INT UNSIGNED NOT NULL,
+		favoriteProductId INT UNSIGNED NOT NULL,
+		favoriteDate DATETIME(6) NOT NULL,
+		-- ^ These are not auto_increment because they're still foreign keys
+		INDEX(favoriteProfileId),
+		INDEX(favoriteProductId),
+	-- ^ Index the foreign keys.
+		FOREIGN KEY(favoriteProfileId) REFERENCES profile(profileId),
+		FOREIGN KEY(favoriteProductId) REFERENCES product(productId),
+	-- ^ Create the foreign key relations.
+		PRIMARY KEY(favoriteProductId, favoriteProductId)
+	-- ^ Finally, create a composite foreign key with the two foreign keys.
+);
